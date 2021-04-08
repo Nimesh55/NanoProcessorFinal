80fe783220013f6826cbd31c5e6b6ea4dbd689ab
Nimesh55 <ariyarathnanimesh@gmail.com>
2021-04-08 17:01:26 +0530

    2_way_3_bit_MUX
    2_way_3_bit_MUX

--- Final_Project/Fianl_Projact/Fianl_Projact.srcs/sources_1/imports/Sources/MUX_2_way_3_bit.vhd
+++ Final_Project/Fianl_Projact/Fianl_Projact.srcs/sources_1/imports/Sources/MUX_2_way_3_bit.vhd
@@ -10,25 +10,39 @@ ENTITY MUX_2_way_3_bit IS
 END MUX_2_way_3_bit;
 
 ARCHITECTURE Behavioral OF MUX_2_way_3_bit IS
+
+    COMPONENT Decoder_1_to_2
+        PORT (
+            I : IN STD_LOGIC;
+            EN : IN STD_LOGIC;
+            Y : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+    END COMPONENT;
+
     COMPONENT tri_state_buffer_3bit
         PORT (
             inputTri : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
             outputTri : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
             EN : IN STD_LOGIC);
     END COMPONENT;
-    SIGNAL NOTSel : STD_LOGIC;
+
+    SIGNAL RegSel : STD_LOGIC_VECTOR (1 DOWNTO 0);
+
 BEGIN
+    Decoder : Decoder_1_to_2
+    PORT MAP(
+        I => Selector,
+        EN => '1',
+        Y => RegSel);
+
     tri_state_buffer_3bit_0 : tri_state_buffer_3bit
     PORT MAP(
         inputTri => Adder_3,
         outputTri => Out_put,
-        EN => NOTSel);
+        EN => RegSel(0));
     tri_state_buffer_3bit_1 : tri_state_buffer_3bit
     PORT MAP(
         inputTri => JUMP_TO,
         outputTri => Out_put,
-        EN => Selector);
-
-    NOTSel <= NOT Selector;
+        EN => RegSel(1));
 
 END Behavioral;
