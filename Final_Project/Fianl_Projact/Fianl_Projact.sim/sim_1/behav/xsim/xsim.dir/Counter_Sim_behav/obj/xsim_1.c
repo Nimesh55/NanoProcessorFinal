28ea0854944a686be1cff093fdee1a938fb1d22d
Nimesh55 <ariyarathnanimesh@gmail.com>
2021-04-08 16:38:25 +0530

    PC_time
    PC_time

--- Final_Project/Fianl_Projact/Fianl_Projact.sim/sim_1/behav/xsim/xsim.dir/Counter_Sim_behav/obj/xsim_1.c
+++ Final_Project/Fianl_Projact/Fianl_Projact.sim/sim_1/behav/xsim/xsim.dir/Counter_Sim_behav/obj/xsim_1.c
@@ -55,8 +55,8 @@ const int NumRelocateId= 5;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/Counter_Sim_behav/xsim.reloc",  (void **)funcTab, 5);
-	iki_vhdl_file_variable_register(dp + 3032);
-	iki_vhdl_file_variable_register(dp + 3088);
+	iki_vhdl_file_variable_register(dp + 2528);
+	iki_vhdl_file_variable_register(dp + 2584);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
