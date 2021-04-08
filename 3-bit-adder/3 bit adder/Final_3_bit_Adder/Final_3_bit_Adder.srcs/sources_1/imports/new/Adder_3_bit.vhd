b29763326ff30c725329a63921f5014ea805fa61
Sathira443 <liyanapathiranasathira@gmail.com>
2021-04-08 16:31:24 +0530

    Adder 3 bit Comments

--- 3-bit-adder/3 bit adder/Final_3_bit_Adder/Final_3_bit_Adder.srcs/sources_1/imports/new/Adder_3_bit.vhd
+++ 3-bit-adder/3 bit adder/Final_3_bit_Adder/Final_3_bit_Adder.srcs/sources_1/imports/new/Adder_3_bit.vhd
@@ -1,47 +1,48 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity Adder_3_bit is
-    Port ( AA : in STD_LOGIC_Vector (2 downto 0); --first number
-           SS : out STD_LOGIC_Vector (2 downto 0) --get result on this
-           ); 
-end Adder_3_bit;
+ENTITY Adder_3_bit IS
+    PORT (
+        AA : IN STD_LOGIC_VECTOR (2 DOWNTO 0); --first number
+        SS : OUT STD_LOGIC_VECTOR (2 DOWNTO 0) --get result on this
+    );
+END Adder_3_bit;
 
-architecture Behavioral of Adder_3_bit is
-component FA --defining full adder circuit with its ports
-port (
-    A: in std_logic;
-    B: in std_logic;
-    C_in: in std_logic;
-    S: out std_logic;
-    C_out: out std_logic);
-end component;
+ARCHITECTURE Behavioral OF Adder_3_bit IS
+    COMPONENT FA --defining full adder circuit with its ports
+        PORT (
+            A : IN STD_LOGIC;
+            B : IN STD_LOGIC;
+            C_in : IN STD_LOGIC;
+            S : OUT STD_LOGIC;
+            C_out : OUT STD_LOGIC);
+    END COMPONENT;
 
-SIGNAL FA0_C, FA1_C, FA2_C : std_logic;
+    SIGNAL FA0_C, FA1_C, FA2_C : STD_LOGIC;
 
-begin
+BEGIN
     FA_0 : FA --full adder 01
-        port map (
+    PORT MAP(
         A => AA(0),
         B => '1', --add 1
         C_in => '0', -- Get selector value to this
         S => SS(0),
         C_Out => FA0_C);
-        
-    FA_1 : FA  -- full adder 02
-        port map (
+
+    FA_1 : FA -- full adder 02
+    PORT MAP(
         A => AA(1),
         B => '0',
         C_in => FA0_C, --input carry bit from FA_0
         S => SS(1),
         C_Out => FA1_C);
-        
-    FA_2 : FA  --full adder 04
-        port map (
+
+    FA_2 : FA --full adder 04
+    PORT MAP(
         A => AA(2),
         B => '0',
         C_in => FA1_C, --input carry bit from FA_2
         S => SS(2),
-        C_Out => FA2_C); 
+        C_Out => FA2_C);
 
-end Behavioral;
+END Behavioral;
